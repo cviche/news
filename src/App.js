b4c447bb2e283f432e816e29b7d4629fac7c506b
Carlos <cviche@ucsc.edu>
2022-01-28 15:40:32 -0800

    Added the Header and News Component to the App Component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from "react";
 import "./App.css";
+import Header from "./components/Header/Header";
+import News from "./components/News/News";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <Header />
+      <div className="news-component-container">
+        <News
+          title="This is the title of the story."
+          image="https://cdn.pixabay.com/photo/2017/01/08/13/58/cube-1963036__340.jpg"
+        />
+        <News
+          title="This is the title of the story."
+          image="https://wallpaperaccess.com/full/154009.jpg"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default App;
