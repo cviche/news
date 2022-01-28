f57384181a672ab4814cfc1b0bd268381c830b6b
Carlos <cviche@ucsc.edu>
2022-01-28 15:40:05 -0800

    Finished the Header Component (js and css)

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="header-container">
+      <div className="header-container-left">
+        <p>Home</p>
+        <p>About</p>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
