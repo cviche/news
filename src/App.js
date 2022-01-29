1552250197135655e0b936dd97491960275276cc
Carlos <cviche@ucsc.edu>
2022-01-29 07:34:49 -0800

    Removed unused imported variable from the App Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
 
