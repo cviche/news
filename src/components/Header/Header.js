ec170f880017718bc4c46f429bde1e034efbf255
Carlos <cviche@ucsc.edu>
2022-01-28 16:09:17 -0800

    Replaced the hard coded links with real react router links

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,17 @@
 import React from "react";
 import "./Header.css";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <div className="header-container">
       <div className="header-container-left">
-        <p>Home</p>
-        <p>About</p>
+        <Link className="header-container-left-link" to="/">
+          Home
+        </Link>
+        <Link className="header-container-left-link" to="/about">
+          About
+        </Link>
       </div>
     </div>
   );
