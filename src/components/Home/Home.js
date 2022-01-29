f3dec11f93b60003829fb7ab70fcb325f4bb57cb
Carlos <cviche@ucsc.edu>
2022-01-29 11:42:51 -0800

    Removed unused file

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import News from "../News/News";
 import "./Home.css";
-import todaysNews from "./Temp.json";
 import { fetchNews, setNewsToken } from "./newsAPI";
 
 function setNewsWrapper(setNews, articles) {
